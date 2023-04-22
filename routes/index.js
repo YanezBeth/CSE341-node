1b0672bff344f63dc795cb151b71601386289454
Beth Yanez <bethyz@byui.edu>
2023-04-21 19:12:10 -0700

    Fixed routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
 
-const lesson1Controller = require('../controllers/lesson1Route');
+const lesson1Controller = require('../controllers/lesson1');
 routes.get('/', lesson1Controller.lesson1Route);
 
 module.exports = routes;
